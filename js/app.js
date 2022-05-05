4f96274af20356f678264d486a45e67c4619aacb
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-05-05 15:10:14 +0100

    adds files

---
+++ js/app.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+
+  const formSubmit = document.querySelector('#new-item-form');
+  formSubmit.addEventListener('submit', handleFormsubmit);
+
+  const deleteAll = document.querySelector('#delete');
+  deleteAll.addEventListener('click', handleDeleteClick);
+});
+
+const handleFormsubmit = function (event) {
+  event.preventDefault();
+  const newBook = document.createElement('li');
+
+  const bookTitle = document.createElement('h3');
+  const bookAuthor = document.createElement('h4');
+  const bookCat = document.createElement('p');
+
+  bookTitle.textContent = event.target.title.value;
+  bookAuthor.textContent = event.target.author.value;
+  bookCat.textContent = event.target.category.value;
+
+  newBook.appendChild(bookTitle);
+  newBook.appendChild(bookAuthor);
+  newBook.appendChild(bookCat);
+
+  newBook.classList.add('book');
+
+  const bookList = document.querySelector('#reading-list');
+  bookList.appendChild(newBook);
+
+  document.getElementById('new-item-form').reset();
+};
+
+const handleDeleteClick = function () {
+  const bookList = document.querySelector('#reading-list');
+  while (bookList.hasChildNodes()) {
+    bookList.removeChild(bookList.firstChild);
+  }
+};
